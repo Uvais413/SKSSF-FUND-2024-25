3b9cb632451f867f0fe4531f0484be5231be1efa
Uvais413 <muhammeduvaisap786@gmail.com>
2025-05-07 07:50:34 +0530

    ok commit

--- data.js
+++ data.js
@@ -1,10 +1,27 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxwRpllURy80nSopFZwAjOrCur3JqE_d-AzXw7DnzvT49hZo9ZnXxkt10NGx_BpfQMC/exec';
-const form = document.forms['contact-form'];
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('messageForm');
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => alert("Thank you! your form is submitted successfully."))
-    .then(() => { window.location.reload(); })
-    .catch(error => console.error('Error!', error.message));
-})
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const formData = new FormData(form);
+
+    fetch(form.action, {
+      method: 'POST',
+      body: formData,
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.result === 'success') {
+        alert('Message sent successfully!');
+        form.reset(); // Clear the form
+      } else {
+        alert('Error sending message. Please try again.');
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert('Error sending message. Please try again.');
+    });
+  });
+});
